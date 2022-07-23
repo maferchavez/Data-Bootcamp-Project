81f129224286d1ac6620a3495adff49b5e663042
Mafer Chávez <maryfer.chavezromo@live.com>
2022-07-23 18:17:05 -0500

    Add postgres connection

--- dags/data_ingestion.py
+++ dags/data_ingestion.py
@@ -2,9 +2,10 @@
 
 from airflow.models import DAG
 from airflow.operators.dummy import DummyOperator #an operator to fill with something, it does not do anything
-from airflow.utils.dates import days_ago
+from airflow.utils.dates import days_ago #used it for the start_date of dag inizialization
+from airflow.providers.postgres.operator.postgres import PostgresOperator # used for prepare task to setup environment for landing data
 
-with DAG("db_ingestion", start_date = days_ago(1)) as dag:#Instanciate and load a DAG object
+with DAG("db_ingestion", start_date = days_ago(1), schedule_interval = '@once') as dag:#Instanciate and load a DAG object
     """
     Defing tasks of a workflow process
         VALIDATE: Validate the presence of source data.
@@ -13,7 +14,10 @@ with DAG("db_ingestion", start_date = days_ago(1)) as dag:#Instanciate and load 
     """
     start_workflow = DummyOperator(task_id='start_workflow') 
     validate = DummyOperator(task_id='validate') 
-    prepare = DummyOperator(task_id='prepare') 
+    prepare = PostgresOperator(
+        task_id='prepare',
+        postgres_conn_id = 'ml_conn',#how to do a connection via airflow?
+        sql = '') 
     load = DummyOperator(task_id='load') 
     end_workflow = DummyOperator(task_id='end_workflow')
 
