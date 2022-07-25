2fde8c035dc9bf97f1a53226645d8a48aa2c3164
Mafer Chávez <maryfer.chavezromo@live.com>
2022-07-25 12:56:54 -0500

    Set SQL query

--- dags/data_ingestion.py
+++ dags/data_ingestion.py
@@ -17,7 +17,18 @@ with DAG("db_ingestion", start_date = days_ago(1), schedule_interval = '@once') 
     prepare = PostgresOperator(
         task_id='prepare',
         postgres_conn_id = 'example',#how to do a connection via airflow?
-        sql = '') 
+        sql = """
+                CREATE A TABLE IF NOT EXISTS user_purchase(
+                    invoice_number varchar(10),
+                    stock_code varchar(20),
+                    detail varchar(1000),
+                    quantity int,
+                    invoice_date timestamp,
+                    unit_price numeric(8,3),
+                    customer_id int,
+                    country varchar(20)
+                            );
+                            """) 
     load = DummyOperator(task_id='load') 
     end_workflow = DummyOperator(task_id='end_workflow')
 
