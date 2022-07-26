9b20084266303a19fa555fc7bc411b12dfb1d84e
Mafer Chávez <maryfer.chavezromo@live.com>
2022-07-26 13:15:42 -0500

    Add S3 to main terraform

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -55,13 +55,13 @@ module "rds" {
   publicly_accessible = var.publicly_accessible
 }
 
-# module "s3" {
-#   source = "./modules/s3"
+ module "s3" {
+   source = "./modules/s3"
 
-#   vpc_id_s3   = module.networking.vpc_id
-#   subnet_s3 = module.networking.private_subnets_ids
+   vpc_id_s3   = module.networking.vpc_id
+   subnet_s3 = module.networking.private_subnets_ids
 
-#   bucket_prefix = var.bucket_prefix
-#   acl           = var.acl
-#   versioning    = var.versioning
-# }
+   bucket_prefix = var.bucket_prefix
+   acl           = var.acl
+   versioning    = var.versioning
+ }
