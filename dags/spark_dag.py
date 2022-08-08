ccb8c519c5e7fcd0e6aa1e5479979e2964371fb2
maferchavez <maryfer.chavezromo@live.com>
2022-08-08 13:46:13 -0500

    edit retry time

--- dags/spark_dag.py
+++ dags/spark_dag.py
@@ -109,7 +109,7 @@ default_args = {
     'email_on_failure': False, 
     'email_on_retry': False, 
     'retries': 1, 
-    'retry_delay': timedelta(minutes=5)
+    'retry_delay': timedelta(minutes=0.5)
 }
 
 with DAG(
