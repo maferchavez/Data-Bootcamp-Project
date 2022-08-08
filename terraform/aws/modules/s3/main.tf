5b9f673dd439403eb36386681da3c789f8e1cb62
maferchavez <maryfer.chavezromo@live.com>
2022-08-07 20:01:02 -0500

    Load pyspark scripts with terrafomr

--- terraform/aws/modules/s3/main.tf
+++ terraform/aws/modules/s3/main.tf
@@ -9,3 +9,11 @@ resource "aws_s3_bucket" "bucket" {
     Name = "s3-data-bootcamp"
   }
 }
+
+resource "aws_s3_bucket_object" "object1" {
+for_each = fileset("C:/Users/maryf/Documents/Data-bootcamp/Data-Bootcamp-Project/dags/spark_scripts/", "*")
+bucket = aws_s3_bucket.bucket.id
+key = each.value
+source = "C:/Users/maryf/Documents/Data-bootcamp/Data-Bootcamp-Project/dags/spark_scripts/${each.value}"
+etag = filemd5("C:/Users/maryf/Documents/Data-bootcamp/Data-Bootcamp-Project/dags/spark_scripts/${each.value}")
+}
